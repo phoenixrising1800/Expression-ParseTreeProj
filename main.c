8dc3730315aa7d22f1b7bddf2aa49f1f0a65489a
Nixy C <phoenixrising1800@gmail.com>
2021-10-05 17:41:42 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+/* Example code from igraph tutorial */
+#include <igraph/igraph.h>
+#include <stdio.h>
+
+int main() {
+    igraph_real_t diameter;
+    igraph_t graph;
+
+    igraph_rng_seed(igraph_rng_default(), 42);
+
+    igraph_erdos_renyi_game(&graph, IGRAPH_ERDOS_RENYI_GNM, 1000, 3000,
+                            IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
+                            
+    igraph_diameter(&graph, &diameter, 0, 0, 0, IGRAPH_UNDIRECTED, 1);
+    printf("Diameter of a random graph with avg. degree %g: %g\n",
+            2.0 * igraph_ecount(&graph) / igraph_vcount(&graph), 
+            (double) diameter);
+
+    igraph_destroy(&graph);
+    // printf("test\n");
+
+    return 0;
+}
